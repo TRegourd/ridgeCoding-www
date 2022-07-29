48ac94537859dc0d2aae78e8b37f8d6bafe685ba
Thomas Regourd <tom.reg@hotmail.fr>
2022-07-29 10:34:41 +0200

    finished contact form

--- src/components/IndexContact.tsx
+++ src/components/IndexContact.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { Trans } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
+import { text } from "stream/consumers";
 import { contactDetails } from "../libs/contactDetails";
 
 function IndexContact() {
+  const { t } = useTranslation();
+
   return (
     <section
       className="relative bg-zinc-50 py-16 dark:bg-gray-800 md:py-24"
@@ -27,62 +30,108 @@ function IndexContact() {
           <div className="lg:col-span-8">
             <div className="rounded-md bg-white p-6 shadow dark:bg-black">
               <form
-                method="post"
-                name="myForm"
-                id="myForm"
-                //onsubmit="return validateForm()"
+                action="https://api.web3forms.com/submit"
+                method="POST"
+                id="form"
               >
-                <p className="mb-0" id="error-msg"></p>
-                <div id="simple-msg"></div>
-                <div className="grid lg:grid-cols-12 lg:gap-6">
-                  <div className="mb-5 lg:col-span-6">
-                    <input
-                      name="name"
-                      id="name"
-                      type="text"
-                      className="form-input"
-                      placeholder="Name :"
-                    />
-                  </div>
+                <input
+                  type="hidden"
+                  name="access_key"
+                  value="e27785ca-c999-463b-9fd9-bf2740975c1b"
+                />
+                <input
+                  type="checkbox"
+                  name="botcheck"
+                  className="hidden"
+                  style={{ display: "none" }}
+                />
 
-                  <div className="mb-5 lg:col-span-6">
-                    <input
-                      name="email"
-                      id="email"
-                      type="email"
-                      className="form-input"
-                      placeholder="Email :"
-                    />
-                  </div>
+                <input
+                  type="hidden"
+                  name="subject"
+                  value="New Submission from Web3Forms"
+                />
+                <input
+                  type="hidden"
+                  name="redirect"
+                  value="https://web3forms.com/success"
+                />
+                <div className="mb-6">
+                  <label
+                    htmlFor="name"
+                    className="mb-2 block text-sm text-gray-600 dark:text-gray-400"
+                  >
+                    <Trans>CONTACT.name</Trans>
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="John Doe"
+                    required
+                    className="w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-300 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:focus:border-gray-500 dark:focus:ring-gray-900"
+                  />
                 </div>
-
-                <div className="grid grid-cols-1">
-                  <div className="mb-5">
-                    <input
-                      name="subject"
-                      id="subject"
-                      className="form-input"
-                      placeholder="Subject :"
-                    />
-                  </div>
-
-                  <div className="mb-5">
-                    <textarea
-                      name="comments"
-                      id="comments"
-                      className="form-input textarea"
-                      placeholder="Message :"
-                    ></textarea>
-                  </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="email"
+                    className="mb-2 block text-sm text-gray-600 dark:text-gray-400"
+                  >
+                    <Trans>CONTACT.email</Trans>
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="john.doe@gmail.com"
+                    required
+                    className="w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-300 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:focus:border-gray-500 dark:focus:ring-gray-900"
+                  />
                 </div>
-                <button
-                  type="submit"
-                  id="submit"
-                  name="send"
-                  className="btn btn-primary flex h-11 items-center justify-center rounded-md"
-                >
-                  <Trans>BUTTON.send_message</Trans>
-                </button>
+                <div className="mb-6">
+                  <label
+                    htmlFor="phone"
+                    className="text-sm text-gray-600 dark:text-gray-400"
+                  >
+                    <Trans>CONTACT.phone</Trans>
+                  </label>
+                  <input
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    placeholder="+33 601020304"
+                    className="w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-300 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:focus:border-gray-500 dark:focus:ring-gray-900"
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="message"
+                    className="mb-2 block text-sm text-gray-600 dark:text-gray-400"
+                  >
+                    <Trans>CONTACT.message</Trans>
+                  </label>
+
+                  <textarea
+                    rows={5}
+                    name="message"
+                    id="message"
+                    placeholder={t("CONTACT.message")}
+                    className="w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-300 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:focus:border-gray-500 dark:focus:ring-gray-900"
+                    required
+                  ></textarea>
+                </div>
+                <div className="mb-6">
+                  <button
+                    type="submit"
+                    className="btn btn-primary flex h-11 items-center justify-center rounded-md"
+                  >
+                    <Trans>BUTTON.send_message</Trans>
+                  </button>
+                </div>
+                <p
+                  className="text-center text-base text-gray-400"
+                  id="result"
+                ></p>
               </form>
             </div>
           </div>
@@ -90,23 +139,23 @@ function IndexContact() {
           <div className="lg:col-span-4">
             <div className="lg:ml-8">
               <div className="flex">
-                <div className="icons mx-auto text-center">
-                  <i className="mdi mdi-phone mb-0 block rounded text-2xl dark:text-white"></i>
+                <div className="flex h-20 w-20 items-center justify-center rounded-xl bg-primary-600/5 align-middle text-3xl text-primary-600 shadow-sm">
+                  <i className="mdi mdi-phone"></i>
                 </div>
 
                 <div className="ml-6 flex-1">
                   <h5 className="mb-2 text-lg font-medium dark:text-white">
                     <Trans>CONTACT.phone</Trans>
                   </h5>
-                  <a href="tel:+152534-468-854" className="text-slate-400">
+                  <a href="tel:+33695025391" className="text-slate-400">
                     {contactDetails.phone}
                   </a>
                 </div>
               </div>
 
               <div className="mt-4 flex">
-                <div className="icons mx-auto text-center">
-                  <i className="mdi mdi-email mb-0 block rounded text-2xl dark:text-white"></i>
+                <div className="flex h-20 w-20 items-center justify-center rounded-xl bg-primary-600/5 align-middle text-3xl text-primary-600 shadow-sm">
+                  <i className="mdi mdi-email"></i>
                 </div>
 
                 <div className="ml-6 flex-1">
--- src/libs/contactDetails.tsx
+++ src/libs/contactDetails.tsx
@@ -1,6 +1,6 @@
 export const contactDetails = {
   email: "contact@ridgeconding.com",
-  phone: "+ 33 123456789",
+  phone: "+33 (0)695025391",
   linkedin: "https://www.linkedin.com/company/ridge-coding/",
   gitHub: "https://github.com/TRegourd",
 };
